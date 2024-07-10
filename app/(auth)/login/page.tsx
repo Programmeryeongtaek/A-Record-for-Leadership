e5c75a61ff5d1fd56b71aeb6519e790b2002e539
오영택 <87858689+Programmeryeongtaek@users.noreply.github.com>
2024-07-11 07:47:16 +0900

    Revert "feat : LoginPage 슬라이드 적용"

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,33 +1,7 @@
-"use client";
-
-import { useRouter } from "next/navigation";
-import React, { FormEvent, useState } from "react";
+import React from 'react';
 
 const LoginPage = () => {
-	const router = useRouter();
-	const [username, setUsername] = useState("");
-	const [password, setPassword] = useState("");
-
-	const handleLogin = (e: FormEvent) => {
-		e.preventDefault();
-		router.push("/");
-	};
-	return (
-		<main className="page left-slide">
-			<h2>로그인</h2>
-			<form onSubmit={handleLogin}>
-				<label htmlFor="username">
-					아이디
-					<input id="username" name="username" type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
-				</label>
-				<label htmlFor="password">
-					<input id="passoword" name="password" type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-					비밀번호
-				</label>
-				<button type="submit">로그인</button>
-			</form>
-		</main>
-	);
+  return <div>LoginPage</div>;
 };
 
 export default LoginPage;
