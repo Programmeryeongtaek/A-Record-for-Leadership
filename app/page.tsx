e9cf034f989ac363b8a924e655da992e6b4ee41b
YeongTaek <yeongtaek2109@gmail.com>
2024-07-11 07:12:12 +0900

    feat : MainPage 구상

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,21 @@
+import { useRouter } from "next/navigation";
+
 export default function Home() {
-  return <>home</>;
+	const router = useRouter();
+
+	const handleSignUpClick = () => {
+		router.push("/signup");
+	};
+
+	const handleLoginClick = () => {
+		router.push("/login");
+	};
+
+	return (
+		<div className="page center">
+			<h1>메인 페이지</h1>
+			<button onClick={handleSignUpClick}>회원가입</button>
+			<button onClick={handleLoginClick}>로그인</button>
+		</div>
+	);
 }
