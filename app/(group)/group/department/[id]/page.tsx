629158c52fb8821292a3de7dacdadddb5a93ab0d
YeongTaek <yeongtaek2109@gmail.com>
2024-07-11 06:26:20 +0900

    style : department UI 구상

--- app/(group)/group/department/[id]/page.tsx
+++ app/(group)/group/department/[id]/page.tsx
@@ -42,9 +42,23 @@ const DepartmentDetailPage = ({ params }: Props) => {
 
 	return (
 		<div>
-			<h1>{departmentInfo.name}</h1>
-			<p>구성원 : {departmentInfo.member}</p>
-			<span>태그 : {departmentInfo.tags}</span>
+			<header>
+				<h1>{departmentInfo.name} 마을에 오신 것을 환영합니다!!</h1>
+				<p>구성원 : {departmentInfo.member}</p>
+				<span>태그 : {departmentInfo.tags}</span>
+			</header>
+			<main className="flex flex-col">
+				<div>
+					<div className="flex justify-between">
+						<h2>가족 현황 및 회의</h2>
+						<button>생성</button>
+					</div>
+					{/* //TODO: Link로 연결 + 노션처럼 소켓을 사용하기 */}
+					<ol>
+						<li>00월 00일 회의</li>
+					</ol>
+				</div>
+			</main>
 		</div>
 	);
 };
