ed6dc176dd236fba5bef673c9d992ab2079f44f5
YeongTaek <yeongtaek2109@gmail.com>
2024-07-11 06:39:21 +0900

    feat : 회원가입 구상

--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -1,3 +1,34 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { FormEvent, useState } from "react";
+
 const SignupPage = () => {
-	return <div>SignUp</div>;
+	const router = useRouter();
+	const [username, setUsername] = useState("");
+	const [password, setPassword] = useState("");
+
+	const handleSignUp = (e: FormEvent) => {
+		e.preventDefault();
+		router.push("/");
+	};
+
+	return (
+		<main className="page right-slide">
+			<h2>회원 가입</h2>
+			<form onSubmit={handleSignUp}>
+				<label htmlFor="usename">
+					<input id="username" name="username" type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
+					아이디
+				</label>
+				<label htmlFor="password">
+					<input id="password" name="password" type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+					비밀번호
+				</label>
+				<button type="submit">회원가입</button>
+			</form>
+		</main>
+	);
 };
+
+export default SignupPage;
